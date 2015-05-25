da3df27683129853d05fc07791afb4935ffb48ac
Trinh Minh Cuong <cuong@techmaster.vn>
2015-05-25 17:38:15 +0700

    Update Tables.sql

--- Server/photobook/SQL/tables.sql
+++ Server/photobook/SQL/tables.sql
@@ -1,14 +1,10 @@
 
 CREATE TABLE photo
 (
-  id serial NOT NULL, -- Primary key
+  id serial PRIMARY KEY, -- Primary key
   title character varying(255) NOT NULL,
-  oldfile character varying(255) NOT NULL,
-  CONSTRAINT "PKey" PRIMARY KEY (id)
+  oldfile character varying(255) NOT NULL
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE photo
-  OWNER TO tom;
-COMMENT ON COLUMN photo.id IS 'Primary key';
